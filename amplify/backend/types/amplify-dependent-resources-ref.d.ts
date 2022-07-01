91ea6bd2387eb7f67a65c1277a3dc38d8b81be18
weswhitney <wesleywhitney@gmail.com>
2022-07-01 12:44:26 -0600

    feat: notifications

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,20 @@
+export type AmplifyDependentResourcesAttributes = {
+    "auth": {
+        "coloradoaware8999a5b0": {
+            "IdentityPoolId": "string",
+            "IdentityPoolName": "string",
+            "UserPoolId": "string",
+            "UserPoolArn": "string",
+            "UserPoolName": "string",
+            "AppClientIDWeb": "string",
+            "AppClientID": "string"
+        }
+    },
+    "analytics": {
+        "coloradoaware": {
+            "Region": "string",
+            "Id": "string",
+            "appName": "string"
+        }
+    }
+}
